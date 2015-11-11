a658e5ee4db0d715066d85746416156ad23cd5d1
Phil Carns <carns@mcs.anl.gov>
2015-11-11 17:26:32 -0500

    example code without snoozer

--- examples/basic-standard.c
+++ examples/basic-standard.c
@@ -9,16 +9,25 @@
 #include <unistd.h>
 #include <abt.h>
 
+/* Expected behavior: the primary ES will block on ABT_thread_join(), waiting
+ * for the ULT (thread_fn()) to complete.  The primary ES has nothing else to
+ * execute during this time, but it will busy spin anyway because that is the
+ * default ABT scheduler behavior.
+ */
+
+void thread_fn(void *arg)
+{
+    /* NOTE: this will block whatever ES it executes on */
+    sleep(5);
+    return;
+}
+
 int main(int argc, char **argv) 
 {
-    int values[4];
-    ABT_thread threads[4];
-    int i;
     int ret;
-    ABT_xstream xstream;
-    ABT_pool pool;
-    ABT_sched sched;
-    ABT_pool_def pool_def;
+    ABT_thread tid;
+    ABT_pool pool2;
+    ABT_xstream xstream2;
     
     ret = ABT_init(argc, argv);
     if(ret != 0)
@@ -27,96 +36,21 @@ int main(int argc, char **argv)
         return(-1);
     }
 
-    ret = ABT_xstream_self(&xstream);
-    if(ret != 0)
-    {
-        fprintf(stderr, "Error: ABT_xstream_self()\n");
-        return(-1);
-    }
+    /* create one additional ES */
+    ABT_xstream_create(ABT_SCHED_NULL, &xstream2);
+    ABT_xstream_get_main_pools(xstream2, 1, &pool2);
 
-#if 0
-    ret = hgargo_pool_get_def(ABT_POOL_ACCESS_MPMC, &pool_def);
-    if(ret != 0)
-    {
-        fprintf(stderr, "Error: hgargo_pool_get_def()\n");
-        return(-1);
-    }
-    ret = ABT_pool_create(&pool_def, ABT_POOL_CONFIG_NULL, &pool);
-    if(ret != 0)
-    {
-        fprintf(stderr, "Error: ABT_pool_create()\n");
-        return(-1);
-    }
+    /* launch a ULT on the new ES that will do nothing except sleep() */
+    ABT_thread_create(pool2, thread_fn, NULL, ABT_THREAD_ATTR_NULL, &tid);
 
-    hgargo_create_scheds(1, &pool, &sched);
+    /* wait on the ULT to complete */
+    ABT_thread_join(tid);
+    ABT_thread_free(&tid);
 
-    ABT_sched_get_data(sched, (void**)(&sched_data));
-    ABT_pool_get_data(pool, (void**)(&pool_data));
+    /* wait on the ES to complete */
+    ABT_xstream_join(xstream2);
+    ABT_xstream_free(&xstream2);
 
-    ret = hgargo_setup_ev(&sched_data->ev);
-    if(ret < 0)
-    {
-        fprintf(stderr, "Error: hgargo_setup_ev()\n");
-        return(-1);
-    }
-    pool_data->ev = sched_data->ev;
-
-    ABT_sched_set_data(sched, sched_data);
-    ABT_pool_set_data(pool, pool_data);
-
-    ret = ABT_xstream_set_main_sched(xstream, sched);
-    if(ret != 0)
-    {
-        fprintf(stderr, "Error: ABT_xstream_set_main_sched()\n");
-        return(-1);
-    }
-
-    /* initialize
-     *   note: address here is really just being used to identify transport 
-     */
-    hgargo_init(NA_FALSE, "tcp://localhost:1234");
-
-    /* register RPC */
-    my_rpc_id = my_rpc_register();
-
-    for(i=0; i<4; i++)
-    {
-        values[i] = i;
-        /* Each fiber gets a pointer to an element of the values array to use
-         * as input for the run_my_rpc() function.
-         */
-        ret = ABT_thread_create(pool, run_my_rpc, &values[i],
-            ABT_THREAD_ATTR_NULL, &threads[i]);
-        if(ret != 0)
-        {
-            fprintf(stderr, "Error: ABT_thread_create()\n");
-            return(-1);
-        }
-
-    }
-
-    /* yield to one of the threads */
-    ABT_thread_yield_to(threads[0]);
-
-    for(i=0; i<4; i++)
-    {
-        ret = ABT_thread_join(threads[i]);
-        if(ret != 0)
-        {
-            fprintf(stderr, "Error: ABT_thread_join()\n");
-            return(-1);
-        }
-        ret = ABT_thread_free(&threads[i]);
-        if(ret != 0)
-        {
-            fprintf(stderr, "Error: ABT_thread_join()\n");
-            return(-1);
-        }
-    }
-
-    hgargo_finalize();
-
-#endif
     ABT_finalize();
 
     return(0);
