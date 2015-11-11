1bf534c1112f285dc4392337954bceba553fd089
Phil Carns <carns@mcs.anl.gov>
2015-11-11 15:05:36 -0500

    skeleton for utility API

---
+++ src/abt-snoozer.c
@@ -0,0 +1,19 @@
+/*
+ * (C) 2015 The University of Chicago
+ * 
+ * See COPYRIGHT in top-level directory.
+ */
+
+#include <abt.h>
+#include <abt-snoozer.h>
+#include "abt-snoozer-internal.h"
+
+int ABT_snoozer_xstream_create(ABT_pool *newpool)
+{
+    return(-1);
+}
+
+int ABT_snoozer_xstream_self_set(ABT_pool *newpool)
+{
+    return(-1);
+}
